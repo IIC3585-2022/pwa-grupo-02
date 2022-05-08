42ac5ace01857cdec2bbcdd95b127cb049cf8e2d
meretamal <meretamal@uc.cl>
2022-05-08 16:56:11 -0400

    add basic caching functionatlity

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,13 +1,42 @@
-self.addEventListener('install', (event) => {
-  console.log('[Service Worker] Installing service worker...', event);
+const STATIC_CACHE = 'static-cache-v1';
+
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(STATIC_CACHE)
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/favicon.svg',
+          '/assets/css/style.css',
+          '/assets/scripts/main.js',
+        ]);
+      })
+  )
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activating service worker...', event);
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== STATIC_CACHE) {
+            console.log('[Service Worker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   console.log('[Service Worker] Fetching something...', event);
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
