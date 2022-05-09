e15f6e4ac19ab8be4ea9974f6fc871581d3b9458
meretamal <meretamal@uc.cl>
2022-05-08 21:48:45 -0400

    refactor using async await

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -7,30 +7,24 @@ const dbPromise = idb.open('instagram-pwa-db', 2, (db) => {
   }
 });
 
-const writeData = (dbStore, data) => {
-  return dbPromise
-    .then((db) => {
-      const tx = db.transaction(dbStore, 'readwrite');
-      const store = tx.objectStore(dbStore);
-      store.put(data);
-      return tx.complete;
-    })
+const writeData = async (dbStore, data) => {
+  const db = await dbPromise;
+  const tx = db.transaction(dbStore, 'readwrite');
+  const store = tx.objectStore(dbStore);
+  store.put(data);
+  return tx.complete;
 }
 
-const readAllData = (dbStore) => {
-  return dbPromise
-    .then((db) => {
-      const tx = db.transaction(dbStore, 'readonly');
-      const store = tx.objectStore(dbStore);
-      return store.getAll();
-    })
+const readAllData = async (dbStore) => {
+  const db = await dbPromise;
+  const tx = db.transaction(dbStore, 'readonly');
+  const store = tx.objectStore(dbStore);
+  return store.getAll();
 }
 
-const clearAllData = (dbStore) => {
-  return dbPromise
-    .then((db) => {
-      const tx = db.transaction(dbStore, 'readwrite');
-      const store = tx.objectStore(dbStore); 
-      store.clear();
-    })
+const clearAllData = async (dbStore) => {
+  const db = await dbPromise;
+  const tx = db.transaction(dbStore, 'readwrite');
+  const store = tx.objectStore(dbStore); 
+  return store.clear();
 }
--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -15,81 +15,65 @@ const STATIC_FILES = [
 ];
 const DYNAMIC_CACHE = 'dynamic-cache-v3';
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(STATIC_CACHE)
-      .then(function(cache) {
-        cache.addAll(STATIC_FILES);
-      })
-  )
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(STATIC_CACHE);
+    cache.addAll(STATIC_FILES);
+  })());
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys()
-      .then(function(keyList) {
-        return Promise.all(keyList.map(function(key) {
-          if (key !== STATIC_CACHE && key !== DYNAMIC_CACHE) {
-            return caches.delete(key);
-          }
-        }));
-      })
-  );
+self.addEventListener('activate', (event) =>  {
+  event.waitUntil((async () => {
+    const keyList = await caches.keys();
+    return Promise.all(keyList.map(function(key) {
+      if (key !== STATIC_CACHE && key !== DYNAMIC_CACHE) {
+        return caches.delete(key);
+      }
+    }));
+  })())
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   const url = 'https://firestore.googleapis.com/v1/projects/igpwa-3d0a9/databases/(default)/documents/images'
   if (event.request.url.indexOf(url) > -1) {
-    event.respondWith(fetch(event.request)
-      .then((response) => {
-        const clonedResponse = response.clone()
-        clonedResponse.json()
-          .then((data) => {
-            data.documents.forEach((image) => {
-              writeData('images', image);
-            });
-          })
-        return response;
-      })
-    );
+    event.respondWith((async () => {
+      const response = await fetch(event.request);
+      const clonedResponse = response.clone();
+      const data = await clonedResponse.json()
+      data.documents.forEach((image) => {
+        writeData('images', image);
+      });
+      return response;
+    })())
   } else {
-    event.respondWith(
-      caches.match(event.request)
-        .then((response) => {
-          if (response) {
-            return response
-          } else {
-            return fetch(event.request)
-              .then((response) => {
-                return caches.open(DYNAMIC_CACHE)
-                  .then((cache) =>  {
-                    cache.put(event.request.url, response.clone());
-                    return response;
-                  })
-              });
-          }
-        })
-    );
+    event.respondWith((async () => {
+      const cachedResponse = await caches.match(event.request);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      const response = await fetch(event.request);
+      const cache = await caches.open(DYNAMIC_CACHE);
+      cache.put(event.request.url, response.clone());
+      return response;
+    })());
   }
 });
 
 self.addEventListener('sync', (event) => {
   if (event.tag === 'sync-new-image') {
-    event.waitUntil(
-      readAllData('sync-images')
-        .then((data) => {
-          Promise.all(data.map(({ id, document }) => {
-            fetch(`https://firestore.googleapis.com/v1/projects/igpwa-3d0a9/databases/(default)/documents/images?documentId=${id}`, {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(document),
-            })
-            .then(() => clearAllData('sync-images'));
-          }));
+    event.waitUntil((async () => {
+      const data = await readAllData('sync-images');
+      await Promise.all(data.map(({ id, document }) => {
+        fetch(`https://firestore.googleapis.com/v1/projects/igpwa-3d0a9/databases/(default)/documents/images?documentId=${id}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(document),
         })
-    );
+      }));
+      clearAllData('sync-images');
+    })());
   }
 })
